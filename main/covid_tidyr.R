add9a8bbd0f9ab98ce8af2e022e21e5a28fb48b0
WizU <withyou@bf6.so-net.ne.jp>
2021-01-25 15:20:16 +0900

    Fix for fork.

--- main/covid_tidyr.R
+++ main/covid_tidyr.R
@@ -2,6 +2,11 @@ rm(list = ls())
 # import tidyverse
 library(tidyverse)
 
+# download covid19 data
+dir.create("data")
+download.file("https://dl.dropboxusercontent.com/s/6mztoeb6xf78g5w/COVID-19.csv",
+              "./data/COVID-19.csv")
+
 # read covid19 data
 covid_19 <- readr::read_csv("./data/COVID-19.csv")
 
